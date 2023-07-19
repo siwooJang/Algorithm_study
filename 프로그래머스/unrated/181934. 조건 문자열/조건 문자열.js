f3381271141325e533ab08a0113d42c8bfa94b20
jang siwoo <88125431+siwooJang@users.noreply.github.com>
2023-07-20 00:00:34 +0900

    [unrated] Title: 조건 문자열, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181934. 조건 문자열/조건 문자열.js
@@ -0,0 +1,16 @@
+function solution(ineq, eq, n, m) {
+    if(ineq === ">"){
+        if(eq === "="){
+           return n >= m ? 1 : 0
+        }else{
+           return n > m ? 1 : 0
+        }
+        
+    }else if(ineq === "<"){
+        if(eq === "="){
+           return n <= m ? 1 : 0
+        }else{
+           return n < m ? 1 : 0
+        }
+    }
+}
