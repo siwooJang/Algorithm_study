484e5095076af19323ffb2437459e9014d779e6a
jang siwoo <88125431+siwooJang@users.noreply.github.com>
2023-08-05 23:58:38 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120844. 배열 회전시키기/배열 회전시키기.js
@@ -0,0 +1,7 @@
+function solution(numbers, direction) {
+    if(direction === "right")
+        numbers.unshift(numbers.pop());
+    else
+        numbers.push(numbers.shift());
+    return numbers;   
+}
