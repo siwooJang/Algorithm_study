73602cdf8838e8cf207ed99d91b648bb8c30e4a3
jang siwoo <88125431+siwooJang@users.noreply.github.com>
2023-08-15 23:57:59 +0900

    [level 0] Title: 369게임, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120891. 369게임/369게임.js
+++ 프로그래머스/lv0/120891. 369게임/369게임.js
@@ -1,10 +1,3 @@
 function solution(order) {
-    let count =0;
-    let num = order.toString().split("")
-    for(let i=0; i<num.length; i++){
-        if(num[i] === '3' || num[i]==='6' || num[i]==='9'){
-            count ++
-        }
-    }
-    return count
+    return order.toString().split("").filter((t) => t === '3' || t === '6' || t === '9').length
 }
