7b7af3c83ad0daa5edc53803c90c80cda36236ef
jang siwoo <88125431+siwooJang@users.noreply.github.com>
2023-08-30 20:04:43 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.js
@@ -0,0 +1,22 @@
+function solution(keyinput, board) {
+    let x = board[0]/2 -1
+    let y = board[1]/2 -1
+    let a = 0; let b =0;
+    for(let i=0; i<keyinput.length;i++){
+        if(keyinput[i]==="left"){
+            if(-a<x)
+                a--
+        }else if (keyinput[i]==="right"){
+            if(a<x)
+                a++
+        }else if (keyinput[i]==="down"){
+            if(-b<y)
+                b--
+        }else if (keyinput[i]==="up"){
+            if(b<y)
+                b++
+        }
+    }
+    let result = [a,b]
+    return result
+}
