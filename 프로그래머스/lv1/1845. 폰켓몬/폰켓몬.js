d9fac1a0a20c3f67501767a78d805822cb0e5ca1
jang siwoo <88125431+siwooJang@users.noreply.github.com>
2023-09-01 00:02:32 +0900

    [level 1] Title: 폰켓몬, Time: 0.82 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.js
@@ -1,14 +1,14 @@
 function solution(nums) {
-    var answer = [];
-    var max = nums.length / 2;
+    let result = []
+    let max = nums.length / 2;
         
     for(let i = 0; i < nums.length; i++) {
-        if(answer.length < max) {
-            if(!answer.includes(nums[i])) {
-                answer.push(nums[i]);
+        if(result.length < max) {
+            if(!result.includes(nums[i])) {
+                result.push(nums[i]);
             }
         }
     }
     
-    return answer.length;    
+    return result.length;    
 }
