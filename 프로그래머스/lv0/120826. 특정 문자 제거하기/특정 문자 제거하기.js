1ed0057bad3f9b8a9ecb9984bdd08dac6695b3d1
jang siwoo <88125431+siwooJang@users.noreply.github.com>
2023-09-03 01:19:29 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120826. 특정 문자 제거하기/특정 문자 제거하기.js
+++ 프로그래머스/lv0/120826. 특정 문자 제거하기/특정 문자 제거하기.js
@@ -1,4 +1,6 @@
 function solution(my_string, letter) {
-    my_string = my_string.split(letter).join("")
-    return my_string
+    // RegExp는 정규표현식을 만듬
+    // g는 글로벌 하게 없애란거. letter가 A-Z이면 /[A-Z]/g 이렇게 됨 
+    let reg = new RegExp(letter,'g') 
+    return my_string.replace(reg,'')
 }
