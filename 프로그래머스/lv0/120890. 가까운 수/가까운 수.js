3c159f0a69d7d0351ac04e137b8411183ad51548
jang siwoo <88125431+siwooJang@users.noreply.github.com>
2023-09-10 21:40:55 +0900

    [level 0] Title: 가까운 수, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120890. 가까운 수/가까운 수.js
+++ 프로그래머스/lv0/120890. 가까운 수/가까운 수.js
@@ -1,14 +1,14 @@
 function solution(array, n) {
     array.sort((a,b) => a-b) // 가장 가까운 수 여러개 일때 , 더 작은수 return 하기 위해 오름차순 정렬
     let min = 101;
-    let num = 0; let idx = 0;
+    let num = 0 , result = 0;
     
-    for(let i=0; i<array.length; i++){
-        num = Math.abs(array[i]-n)
+    for(let i of array){
+        num = Math.abs(n-i)
         if ( min > num){
             min = num
-            idx = i
+            result = i
         }
     }
-    return array[idx]
+    return result
 }
