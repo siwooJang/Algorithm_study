8904fbd3609dd70aab1f3ed1eac7d0061c3db24d
jang siwoo <88125431+siwooJang@users.noreply.github.com>
2023-09-16 23:42:20 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.16 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120853. 컨트롤 제트/컨트롤 제트.js
+++ 프로그래머스/lv0/120853. 컨트롤 제트/컨트롤 제트.js
@@ -5,7 +5,7 @@ function solution(s) {
         if(i==="Z"){
             result.pop()
         } else{
-            result.push(+i)
+            result.push(+i) // 숫자로 변환해서 넣으려고 +
         }
     }
     return result.reduce((a,c)=>a+c,0)
