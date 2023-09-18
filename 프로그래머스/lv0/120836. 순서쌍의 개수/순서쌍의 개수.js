234048b3f95e40c77ba73a6fe39ce771d62f5d2e
jang siwoo <88125431+siwooJang@users.noreply.github.com>
2023-09-18 11:07:55 +0900

    [level 0] Title: 순서쌍의 개수, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.js
+++ 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.js
@@ -1,9 +1,7 @@
 function solution(n) {
-    let result = []
-    for(let i=0; i<n; i++){
-        if(n%i===0){
-            result.push(i)
-        }
+    let cnt = 0;
+    for(let i =1; i*i<n; i++){
+        if(n%i===0) cnt++;
     }
-    return result.length+1
+    return Number.isInteger(Math.sqrt(n)) ? 2*cnt+1 : 2*cnt
 }
