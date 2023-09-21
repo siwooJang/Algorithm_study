b2840ec89bfe79d7ab17c379acdeddfa8400ca63
jang siwoo <88125431+siwooJang@users.noreply.github.com>
2023-09-21 23:59:36 +0900

    [level 0] Title: 다항식 더하기, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120863. 다항식 더하기/다항식 더하기.js
+++ 프로그래머스/lv0/120863. 다항식 더하기/다항식 더하기.js
@@ -1,22 +1,22 @@
 function solution(polynomial) {
-    
-    let arr = polynomial.split(" + ")
-    let x = 0; let num = 0;
-    
-    arr.forEach((n) => {
-        if(n.includes("x")){
-            let str = n.replace("x","") || "1"
-            x += parseInt(str,10)
-        } else {
-            num += parseInt(n)
-        }
-    })
-    
-    let answer = []
-    
-    if(x) answer.push(`${x === 1 ? "" : x}x`)
-    if(num) answer.push(num)
-    
-    return answer.join(" + ")
+  const arr = polynomial.split(" + ");
 
+  let xNum = 0;
+  let num = 0;
+
+  arr.forEach((n) => {
+    if (n.includes("x")) {
+      const strVal = n.replace("x", "") || "1";  // 여기가 문제였다. replace 생각도 못함.
+      xNum += parseInt(strVal, 10);
+    } else {
+
+      num += parseInt(n);
+    }
+  });
+
+  let answer = [];
+
+  if (xNum) answer.push(`${xNum === 1 ? "" : xNum}x`);
+  if (num) answer.push(num);
+  return answer.join(" + ");
 }
