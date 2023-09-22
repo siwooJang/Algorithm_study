286d1035a6f2c008bcbdd2cba98401ad375d9a89
jang siwoo <88125431+siwooJang@users.noreply.github.com>
2023-09-22 23:01:45 +0900

    [level 0] Title: 안전지대, Time: 0.40 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120866. 안전지대/안전지대.js
@@ -0,0 +1,28 @@
+function solution(board) {
+  let answer = 0;
+	
+  const dangerArea = [
+		[-1, 0], [1, 0], [0, 1], [0, -1], [-1, 1], [1, 1], [1, -1], [-1, -1]
+	]; // 위험지역 xy좌표
+	
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board[i].length; j++) {
+      if (board[i][j] == 1) {
+        dangerArea.map((v) => {
+          let [x, y] = v;
+          [x, y] = [x + i, y + j];
+          if (
+            x >= 0 &&
+            x < board.length &&
+            y >= 0 &&
+            y < board[i].length &&
+            board[x][y] == 0
+          )
+            board[x][y] = 2;
+        });
+      }
+    }
+  }
+  board.map(v1 => v1.map(v2 => v2 == 0 ? answer++ : 0));
+  return answer;
+}
