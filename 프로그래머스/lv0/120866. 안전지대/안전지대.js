3e9f894ed140732e90d17c83e2ce40746e5babd8
jang siwoo <88125431+siwooJang@users.noreply.github.com>
2023-09-22 23:08:13 +0900

    [level 0] Title: 안전지대, Time: 0.15 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120866. 안전지대/안전지대.js
+++ 프로그래머스/lv0/120866. 안전지대/안전지대.js
@@ -3,11 +3,16 @@ function solution(board) {
     let outside = [[-1,0], [-1,-1], [-1,1], [0,-1],[0,1],[1,0], [1,-1], [1,1]];
     let safezone = 0;
 
-    board.forEach((row, y, self) => row.forEach((it, x) => {
-        if (it === 1) return false;
-        return outside.some(([oy, ox]) => !!self[oy + y]?.[ox + x])
-               ? false : safezone++;
-    }));
+    // board.forEach((row, y, self) => row.forEach((it, x) => {
+    //     if (it === 1) return false;
+    //     return outside.some(([oy, ox]) => !!self[oy + y]?.[ox + x]) // !!self[oy + y] && !!self[oy + y][ox + x]
+    //            ? false : safezone++;
+    // }));
+    
+    board.forEach((row,y,self)=> row.forEach((it,x)=>{
+        if (it === 1) return 0;
+        return outside.some(([oy,ox])=>!!self[oy+y]&&!!self[oy+y][ox+x]) ? 0 : safezone++;
+    }))
 
     return safezone;
 }
