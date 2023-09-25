7c6763df29fd7ee6c07c5778c2f41047ce59547c
jang siwoo <88125431+siwooJang@users.noreply.github.com>
2023-09-25 23:54:59 +0900

    [level 0] Title: 특이한 정렬, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120880. 특이한 정렬/특이한 정렬.js
+++ 프로그래머스/lv0/120880. 특이한 정렬/특이한 정렬.js
@@ -2,8 +2,8 @@ function solution(numlist, n) {
     return numlist.sort((a,b)=>{
         let [aGAP,bGAP] = [Math.abs(n-a),Math.abs(n-b)];
         if(aGAP===bGAP){
-            return b-a;
+            return b-a; // 거리가 같다면 오름차순
         } 
-        return aGAP - bGAP
+        return aGAP - bGAP // 거리가 다르다면 거리순으로 오름차순
     })
 }
