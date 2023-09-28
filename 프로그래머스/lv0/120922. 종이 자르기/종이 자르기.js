8f9f51b3954f62d54a53e8a642db0819cfd63954
jang siwoo <88125431+siwooJang@users.noreply.github.com>
2023-09-28 23:51:12 +0900

    [level 0] Title: 종이 자르기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120922. 종이 자르기/종이 자르기.js
@@ -0,0 +1,10 @@
+function solution(M, N) {
+    if(M>1 && N>1){
+        return (N-1) + (M-1)*N
+    } else if(M>1 || N>1){
+        return (N-1) + (M-1)*N
+    } else{
+        return 0
+    }
+
+}
