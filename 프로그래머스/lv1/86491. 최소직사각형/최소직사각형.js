d1b07f235c266fcde041ed5763221fde94eca66c
siwooJang <j35857834@gmail.com>
2023-10-06 00:15:11 +0900

    update

--- 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.js
+++ 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.js
@@ -1,5 +1,5 @@
 function solution(sizes) {
-    const [hor, ver] = sizes.reduce(([h, v], [a, b]) => 
+    const [hr, ver] = sizes.reduce(([h, v], [a, b]) => 
     [Math.max(h, Math.max(a, b)), Math.max(v, Math.min(a, b))], [0, 0])
-    return hor * ver;
+    return hr * ver;
 }
