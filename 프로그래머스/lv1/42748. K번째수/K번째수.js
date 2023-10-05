c4ff66dd2520e0c451f7225d7f504b19adf48aa7
jang siwoo <88125431+siwooJang@users.noreply.github.com>
2023-10-06 00:01:04 +0900

    [level 1] Title: K번째수, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/42748. K번째수/K번째수.js
+++ 프로그래머스/lv1/42748. K번째수/K번째수.js
@@ -1,17 +1,8 @@
 function solution(array, commands) {
-    const answer = [];
-    let i = 0;
-    let j = 0;
-    let k = 0;
-    for (let m = 0; m < commands.length; m++) {
-        i = commands[m][0]
-        j = commands[m][1]
-        k = commands[m][2]
-        
-        let sliced = array.slice(i-1,j)
-        let sorted = sliced.sort((a,b)=> a - b)
-    
-        answer.push(sorted[k-1])
-    }
+    var answer = [];
+
+    answer = commands.map(a=>{
+        return array.slice(a[0]-1,a[1]).sort((b,c)=>b-c)[a[2]-1];
+    })
     return answer;
 }
