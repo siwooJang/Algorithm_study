3e732cb30c545e419d87e11f50f2b0ffd70393a5
jang siwoo <88125431+siwooJang@users.noreply.github.com>
2023-11-05 03:16:09 +0900

    [level 1] Title: 대충 만든 자판, Time: 0.96 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.js
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.js
@@ -1,26 +1,11 @@
 function solution(keymap, targets) {
-    const answer = new Array(targets.length).fill(-1);
-    
-    // targets을 탐색
-    for(let i = 0; i < targets.length; i++) {
-        const target = targets[i];
-        let count = 0;
-        // target의 각 문자를 탐색
-        for(let j = 0; j < target.length; j++) {
-            const str = target[j];
-            // keymap의 각 요소 중 target[j]를 포함하는 문자의 minimum index 구함
-            const min = Math.min(...keymap.map(x => {
-                const idx = x.indexOf(str);
-                return idx === -1 ? Infinity : idx + 1;
-            }));
-            if(min === Infinity) {
-                count = -1;
-                break;
-            }
-            count += min;
-        }
-        answer[i] = count;
+    const answer = [];
+    const map = {}
+    for (const items of keymap) {
+        items.split('').map((item, index) => map[item] = (map[item] < index+1 ? map[item] : index+1))
     }
-    
+    for (const items of targets) {
+        answer.push(items.split('').reduce((cur, item) => cur += map[item], 0) || -1)
+    }
     return answer;
 }
