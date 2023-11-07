c240a5171561b2cd615e1a42a787d3a8103bbb8b
jang siwoo <88125431+siwooJang@users.noreply.github.com>
2023-11-08 00:24:59 +0900

    [level 2] Title: H-Index, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.js
@@ -0,0 +1,15 @@
+function solution(citations) {
+    let result = 0;
+    let s = citations.sort((a,b)=>b-a)
+    for(let i=0; i<s.length;i++){
+        if(s[i]>i){
+            result++
+        }
+    }
+    return result
+}
+//논문 인용횟수를 내림차순으로 정렬하고 배열 길이만큼 for문을 돌린다.
+
+// 자신의 인용횟수(citations[i])가 자신보다 인용횟수가 많은 논문 수(i) 보다 많으면 정답 수를 1 증가시킨다.
+
+//자신의 인용횟수가 자신보다 인용횟수가 많은 논문 수와 같아 질때 의 answer를 return하면 정답
