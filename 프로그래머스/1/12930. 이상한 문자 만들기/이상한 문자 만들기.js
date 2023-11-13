32dd75d9c3d3c5ebfc172a7fac26481cfcc2fd6f
jang siwoo <88125431+siwooJang@users.noreply.github.com>
2023-11-13 23:55:16 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.18 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
@@ -1,16 +1,17 @@
 function solution(s) {
-  const str = s.split(" ");
-  var answer = [];
-
-  let tmp = "";
-  str.forEach((element) => {
-    for (let i = 0; i < element.length; i++) {
-      tmp += i % 2 == 0 ? element[i].toUpperCase() : element[i].toLowerCase();
+    let answer = [];
+    let result = s.split(" ")
+    
+    for ( let i =0; i<result.length; i++){
+        let sum = ''
+        for(let j =0; j<result[i].length;j++){
+            if (j%2===0){
+                sum += result[i][j].toUpperCase()
+            } else {
+                sum += result[i][j].toLowerCase()
+            }
+        }
+        answer.push(sum)
     }
-
-    answer.push(tmp);
-    tmp = "";
-  });
-
-  return answer.join(" ");
+    return answer.join(' ')
 }
