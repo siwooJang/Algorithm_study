092457eabe1139b1b5c9ada00aac840ace724c67
jang siwoo <88125431+siwooJang@users.noreply.github.com>
2023-11-22 23:21:37 +0900

    [level 2] Title: 전화번호 목록, Time: 126.39 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -1,9 +1,10 @@
 function solution(phone_book) {
-    let result = []
-    let a = phone_book.sort();
-    for(let i =0; i<a.length-1; i++){
-        if(a[i] === a[i+1].substring(0,a[i].length))
+    let result = [];
+    let sort = phone_book.sort()
+    for(let i =0; i<sort.length-1;i++){
+        if(sort[i]===sort[i+1].substring(0,sort[i].length)){
             return false
+        }
     }
     return true
 }
