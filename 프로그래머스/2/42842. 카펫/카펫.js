b5cf4ce3ef107dd7179ca946be435b08562c541d
jang siwoo <88125431+siwooJang@users.noreply.github.com>
2023-12-06 23:19:11 +0900

    [level 2] Title: 카펫, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.js
+++ 프로그래머스/2/42842. 카펫/카펫.js
@@ -1,8 +1,9 @@
 function solution(brown, yellow) {
     // 조건 1 : (brown + yellow) / x = y
     // 조건 2 : (x-2) * (y-2) === yellow
-    for (let y = 3; y<brown+yellow;y++){
-        let x = (brown+yellow) / y;
+    let a = brown+yellow;
+    for (let y = 3; y<a;y++){
+        let x = a / y;
         if((x-2) * (y-2) === yellow){ 
             return [x,y]
         }
