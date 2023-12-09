88c5c6bb261e7044d1f45f89da0eb598dcb9ad00
jang siwoo <88125431+siwooJang@users.noreply.github.com>
2023-12-09 23:42:11 +0900

    [level 2] Title: 모음 사전, Time: 1.92 ms, Memory: 37 MB -BaekjoonHub

---
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.js
@@ -0,0 +1,17 @@
+function solution(word) {
+  const result = [];
+  const str = "";
+  for (let i = 1; i <= 5; i++) dfs(str, i, result);
+  return result.sort().indexOf(word) + 1;
+}
+
+const dfs = (word, length, result) => {
+  const vowels = [..."AEIOU"];
+  if (length === word.length) {
+    result.push(word);
+    return;
+  }
+  vowels.forEach((vowel) => {
+    dfs(word + vowel, length, result);
+  });
+};
