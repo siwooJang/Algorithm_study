3989968147794adc4cb8e3cd1fa31335d6fb4152
jang siwoo <88125431+siwooJang@users.noreply.github.com>
2023-12-11 00:02:36 +0900

    [level unrated] Title: [PCCP 기출문제] 1번 / 붕대 감기, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.js
+++ 프로그래머스/unrated/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.js
@@ -1,35 +1,20 @@
 function solution(bandage, health, attacks) {
-    const [time, heal, bonus] = bandage;
-    const len = attacks[attacks.length - 1][0];
-    let cntSuccess = 0;
-    let curHp = health; // 캐릭터 체력
-    let attackIdx = 0; // 현재 공격 정보
+    let isDead = false;
+    let hp = health;
+    let timing = 0;
+    
+    attacks.forEach(([a_timing, damage]) => {
+        let duration = a_timing - timing - 1;
+        let nextHp = hp + (duration * bandage[1] 
+                    + (Math.floor(duration / bandage[0]) * bandage[2]));
 
-    for (let i = 1; i <= len; i++) {
-        // 체력이 다 소진 된 경우
-        if (curHp <= 0) return -1;
-
-        // 공격시간이 된 경우
-        if (attackIdx < attacks.length && i === attacks[attackIdx][0]) {
-            curHp -= attacks[attackIdx][1];
-            cntSuccess = 0;
-            attackIdx++; // 다음 공격 정보로 이동
-            continue;
-        }
-
-        // 공격시간이 아니면 체력 회복
-        curHp += heal;
-        cntSuccess++;
-
-        if (cntSuccess === time) {
-            curHp += bonus;
-            cntSuccess = 0;
-        }
-
-
-        // curHp가 원래 체력 보다 넘어간 경우 다시 원래 체력으로 재할당
-        if (curHp > health) curHp = health;
-    }
-
-    return curHp <= 0 ? -1 : curHp;
+        timing = a_timing;
+        
+        hp = nextHp > health ? health : nextHp;
+        hp -= damage;
+        
+        if (hp < 1) isDead = true;
+    })
+    
+    return isDead ? -1 : hp;
 }
