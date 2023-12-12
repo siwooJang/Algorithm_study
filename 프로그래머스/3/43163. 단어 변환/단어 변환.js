44ffbdd2a4ef46ceda8123d5748639ee5ea0ccff
jang siwoo <88125431+siwooJang@users.noreply.github.com>
2023-12-12 23:55:58 +0900

    [level 3] Title: 단어 변환, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.js
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.js
@@ -1,29 +1,35 @@
 function solution(begin, target, words) {
-  const visited = { [begin] : 0 };
-  const queue = [begin];
+    let answer = 0;
+    let visited = [];
+    let queue = [];
 
-  while(queue.length) {
-    const cur = queue.shift();
-    
-    if(cur === target) break;
-    
-    for(const word of words) {
-      if(isConnected(word, cur) && !visited[word]) {
-        visited[word] = visited[cur] + 1;
-        queue.push(word);
-      }
+    if(!words.includes(target)) return 0; //words배열에 target이 없으면 0을 반환
+        
+    queue.push([begin,answer]); //일단 queue에 [시작단어, 변경횟수]를 배열형태로 넣는다.
+
+    while(queue) {
+        let [v, cnt] = queue.shift(); //queue의 맨 왼쪽 값을 꺼낸다.
+
+        if (v === target) { //꺼낸값의 v가 맞으면 cnt는 횟수 이므로 cnt를 return한다.
+            return cnt;
+        }
+
+        words.forEach(word => {
+            let notEqual = 0; //다른갯수를 구하기위해 변수를 선언한다.
+            
+            if(visited.includes(word)) return; //방문했던 단어면 pass
+
+            for (let i=0; i<word.length; i++) {
+                if (word[i] !== v[i]) notEqual++; //word를 반복하면서 다른 알파벳의 갯수를 체크한다
+            }
+            
+            if (notEqual === 1) { //만약 다른게 1개라면
+                queue.push([word, ++cnt]); //queue에 [단어, 횟수] 형태로 넣는다.
+              	//처음으로 따지면 hit -> hot이 되었을 때가 1이 된다.
+                visited.push(word); //방문처리를 해준다.
+            }
+        });
     }
-  }
-  return visited[target] ? visited[target] : 0;
-}
-
-const isConnected = (str1, str2) => {
-  let count = 0;
-  const len = str1.length;
-  
-  for(let i = 0; i < len; i++) {
-    if(str1[i] !== str2[i]) count++;
-  }
-  
-  return count === 1 ? true : false;
+    
+    return answer;
 }
