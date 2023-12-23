dddcd16e953b739700d6b90cf36481dbbf363316
jang siwoo <88125431+siwooJang@users.noreply.github.com>
2023-12-23 23:53:07 +0900

    [level 2] Title: 가장 큰 수, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
@@ -1,7 +1,7 @@
 function solution(numbers) {
-    const answer = numbers.map(v => String(v))
+    const ans = numbers.map(v => String(v))
     .sort((a,b)=> (b+a) - (a+b)) // 가장 큰 조합부터의 내림차순
     .join('');
     
-    return answer[0] === '0'? '0' : answer
+    return ans[0] === '0'? '0' : ans
 }
