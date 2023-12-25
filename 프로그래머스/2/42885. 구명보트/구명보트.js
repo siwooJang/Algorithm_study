209d13719907798f93b61111315315e4849f5fec
jang siwoo <88125431+siwooJang@users.noreply.github.com>
2023-12-26 00:02:57 +0900

    [level 2] Title: 구명보트, Time: 10.43 ms, Memory: 38.1 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.js
+++ 프로그래머스/2/42885. 구명보트/구명보트.js
@@ -4,13 +4,13 @@ function solution(people, limit) {
     let [l,r] = [0,people.length - 1];
     while(l<=r){
         let sum = people[l]+people[r]
-        if(sum>limit){
+        if(sum>limit){ // limit를 넘으면 마지막수(큰수)가 혼자 타는 배, r--
             r--;
-        } else {
+        } else { // limit을 넘지 않으면 (첫번째 수와 마지막 수를 배에 태우기 때문에) 양 끝에서 다음 수로 향하도록 l++, r--
             l++;
             r--;
         }
-        answer++ 
+        answer++  // 혼자 타든 같이 타든 배 횟수 추가 answer ++
     }
 
     return answer
