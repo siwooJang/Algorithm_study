e04e83b22122250a3bf5ad5540c538568a13a338
jang siwoo <88125431+siwooJang@users.noreply.github.com>
2023-12-26 00:01:03 +0900

    [level 2] Title: 구명보트, Time: 11.41 ms, Memory: 38.1 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.js
+++ 프로그래머스/2/42885. 구명보트/구명보트.js
@@ -1,20 +1,16 @@
 function solution(people, limit) {
-    people.sort((a,b)=>b-a)
+    people.sort((a, b) => a - b)
     let answer = 0
     let [l,r] = [0,people.length - 1];
-    while(l<r){
+    while(l<=r){
         let sum = people[l]+people[r]
-        if(sum<=limit){
-            l++;
+        if(sum>limit){
             r--;
         } else {
             l++;
+            r--;
         }
-        answer++
-    }
-    
-    if(l===r){
-        answer++
+        answer++ 
     }
 
     return answer
