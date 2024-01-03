5f9ff885f58869e5ab1dc13c758b912fd1aed30e
jang siwoo <88125431+siwooJang@users.noreply.github.com>
2024-01-03 22:59:43 +0900

    Time: 148 ms (80.06%), Space: 51.1 MB (90.82%) - LeetHub

--- 1472-design-browser-history/1472-design-browser-history.js
+++ 1472-design-browser-history/1472-design-browser-history.js
@@ -1,26 +1,48 @@
+// 1. DLLNode class 정의
+class DLLNode {
+    constructor(url) {
+        // url string을 저장할 변수 선언
+        this.data = url;
+        // prev, next node를 가리키는 pointers 선언
+        this.prev = null;
+        this.next = null;
+    }
+}
 
-var BrowserHistory = function(homepage) {
-    this.arr = [homepage]
-    this.head = 0
+// 2. BrowserHistory class 정의
+class BrowserHistory {
+    constructor(homepage) {
+        // Head node를 가리키는 변수에 DLLNode의 instance를 할당
+        this.head = new DLLNode(homepage);
+        // Current URL node를 가리키는 current 변수 선언
+        this.current = this.head;
+    }
 
-};
+    visit(url) {
+        // 입력된 url을 기반으로 새로운 node를 생성
+        const new_node = new DLLNode(url);
+        // current node의 next가 새로운 node를 가리키도록 한다.
+        this.current.next = new_node;
+        // 새로운 node의 prev가 current node를 가리키도록 한다.
+        new_node.prev = this.current;
+        // 그 후 current로 새로운 node를 가리킨다.
+        this.current = new_node;
+    }
 
-BrowserHistory.prototype.visit = function(url) {
-    this.head++
-    this.arr[this.head] = url
-    this.arr.length = this.head + 1
-};
+    back(steps) {
+        while (steps > 0 && this.current.prev !== null) {
+            this.current = this.current.prev;
+            steps--;
+        }
+        // 변경된 current URL을 리턴한다.
+        return this.current.data;
+    }
 
-
-BrowserHistory.prototype.back = function(steps) {
-    this.head -= steps
-    if (this.head < 0) this.head = 0
-    return this.arr[this.head]
-};
-
-
-BrowserHistory.prototype.forward = function(steps) {
-    this.head += steps
-    if (this.head >= this.arr.length) this.head = this.arr.length - 1
-    return this.arr[this.head]
-};
+    forward(steps) {
+        while (steps > 0 && this.current.next !== null) {
+            this.current = this.current.next;
+            steps--;
+        }
+        return this.current.data;
+    }
+}
