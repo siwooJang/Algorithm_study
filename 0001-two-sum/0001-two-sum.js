f831e8ca82166c73564df2fc3ae606e322ba675a
jang siwoo <88125431+siwooJang@users.noreply.github.com>
2024-01-06 15:21:36 +0900

    Time: 54 ms (84.98%), Space: 43.3 MB (25.74%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -1,9 +1,14 @@
-var twoSum = function(nums, target) {
-    for(var i=0;i<nums.length;i++){
-        for(var j = i+1;j<nums.length;j++){
-            if(nums[i]+nums[j] == target){
-                return [i,j]
-            }
+const twoSum = function(nums, target) {
+    const vMap = new Map();
+    
+    for (let i = 0; i < nums.length; i++) {
+        const complement = target - nums[i];
+      
+        if (vMap.get(complement) !== undefined) {
+          return ([vMap.get(complement), i])
+        } else {
+          vMap.set(nums[i], i)
         }
     }
+    
 };
