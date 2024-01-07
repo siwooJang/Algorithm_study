5c8bbea88176fa7844bebb463262b3d827e9630a
jang siwoo <88125431+siwooJang@users.noreply.github.com>
2024-01-07 23:43:10 +0900

    Time: 62 ms (32.76%), Space: 42 MB (79.15%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -0,0 +1,26 @@
+
+var isValid = function(s) {
+
+    const stack = [];
+    const parens = '() {} []';
+    let i = 0;
+    
+    while(i<s.length){
+        stack.push(s[i]);
+        i++;
+        
+        let open = stack[stack.length-2];
+        let closed = stack[stack.length-1];
+        
+        let potParens = open+closed;
+        
+        if(parens.includes(potParens)){
+            stack.pop();
+            stack.pop();
+        }
+        
+        
+    }
+    return stack.length === 0;
+    
+};
