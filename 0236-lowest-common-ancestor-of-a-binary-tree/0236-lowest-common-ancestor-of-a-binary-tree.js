239784ffdb2ecf709a1d6ec4f9ac3f4ab4a6ae14
jang siwoo <88125431+siwooJang@users.noreply.github.com>
2024-01-12 00:35:07 +0900

    Time: 63 ms (85.02%), Space: 50.7 MB (98.76%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.js
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.js
@@ -1,20 +1,28 @@
-
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
 var lowestCommonAncestor = function(root, p, q) {
-    let result = null;
     
-    let dfs = (node) => {
-        if(node === null) return false;
+    function dfs(node){
+        if(node === null) return null;
+        if (node === p || node === q) return node;
         
         let left = dfs(node.left);
         let right = dfs(node.right);
         
-        let cur = node === p || node === q;
-        
-        if(left+right+cur >= 2) result = node;
-        
-        return left || right || cur;
+        if(left&&right) return node;
+        return left || right;
     }
     
-    dfs(root);
-    return result
+    return dfs(root)
 };
