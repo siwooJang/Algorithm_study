d894374aec52246c610264f4da324c74b6df6c9f
jang siwoo <88125431+siwooJang@users.noreply.github.com>
2024-01-29 23:48:51 +0900

    Time: 59 ms (17.41%), Space: 48.8 MB (5.65%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.js
+++ 0070-climbing-stairs/0070-climbing-stairs.js
@@ -3,12 +3,16 @@
  * @return {number}
  */
 var climbStairs = function(n) {
-    const memo = {1:1,2:2};
+    const memo = {}
     const dfs = (n) => {
-        for(let i =3; i<n+1; i++){
-            memo[i] = memo[i-1] + memo[i-2]
+        if(n===1) return 1;
+        if(n===2) return 2;
+        if(!(n in memo)){
+            memo[n] = dfs(n-1) + dfs(n-2)
         }
         return memo[n]
     }
+
     return dfs(n)
+
 };
