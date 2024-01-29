d88d155c6135fb2f49e676b5e09f1a7dc07c5063
jang siwoo <88125431+siwooJang@users.noreply.github.com>
2024-01-30 00:26:16 +0900

    Time: 58 ms (53.60%), Space: 50.5 MB (8.72%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.js
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.js
@@ -4,8 +4,8 @@
  */
 var minCostClimbingStairs = function(cost) {
     const length = cost.length;
-    const dp = Array(length+1).fill(0)
-    
+    const dp = Array(length+1).fill(-1);
+    dp[0] = 0; dp[1]=0;
     for(let i=2;i<length+1;i++){
         dp[i] = Math.min(dp[i-1]+cost[i-1],dp[i-2]+cost[i-2])
     }
