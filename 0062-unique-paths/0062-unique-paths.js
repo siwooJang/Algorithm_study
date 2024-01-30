53a726390f9c1a7fa1237a48dbe15a9ee2c5d8a9
jang siwoo <88125431+siwooJang@users.noreply.github.com>
2024-01-31 01:23:42 +0900

    Time: 60 ms (28.18%), Space: 49.1 MB (10.14%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.js
+++ 0062-unique-paths/0062-unique-paths.js
@@ -4,33 +4,29 @@
  * @return {number}
  */
 var uniquePaths = function(m, n) {
-    const memo = {}; // 중복 계산을 피하기 위한 메모이제이션 객체
+    // memo 선언 및 초기화
+    const memo = Array.from({ length: m }, () => Array(n).fill(-1));
 
-    // dp 함수: 현재 위치에서 목표 지점까지의 가능한 경로 수를 반환하는 재귀 함수
-    const dp = (row, col) => {
-        // 맨 왼쪽 열 또는 맨 위 행에 도달하면 1을 반환
-        if (row === 0 || col === 0) {
-            return 1;
+    const dp = (r, c) => {
+        if (r === 0 && c === 0) {
+            memo[r][c] = 1; // memo에 값 넣기
+            return memo[r][c];
         }
 
-        const key = row + ',' + col; // 메모이제이션을 위한 키 생성
-
-        // 이미 계산한 결과가 있다면 그 값을 반환
-        if (memo[key] !== undefined) {
-            return memo[key];
+        // memo 중복 제거
+        if (memo[r][c] === -1) {
+            let uniquePaths = 0;
+            if (r - 1 >= 0) {
+                uniquePaths += dp(r - 1, c);
+            }
+            if (c - 1 >= 0) {
+                uniquePaths += dp(r, c - 1);
+            }
+            // memo에 값 넣기
+            memo[r][c] = uniquePaths;
         }
-
-        // 위쪽과 왼쪽으로 이동한 경우의 수를 재귀적으로 계산
-        const fromTop = dp(row - 1, col);
-        const fromLeft = dp(row, col - 1);
-
-        // 계산한 결과를 메모이제이션
-        memo[key] = fromTop + fromLeft;
-
-        return memo[key];
+        return memo[r][c];
     };
 
-    // 로봇은 오른쪽과 아래쪽으로만 이동할 수 있으므로
-    // 목표 지점에서 위쪽과 왼쪽으로 이동하는 경우의 수는 고려하지 않습니다.
     return dp(m - 1, n - 1);
 };
