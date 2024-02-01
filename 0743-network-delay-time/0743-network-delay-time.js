4fe7834b0fa3a6dda18e14c6b2bed6a94381d07f
jang siwoo <88125431+siwooJang@users.noreply.github.com>
2024-02-01 23:32:24 +0900

    Time: 152 ms (29.81%), Space: 61.8 MB (8.45%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.js
+++ 0743-network-delay-time/0743-network-delay-time.js
@@ -5,20 +5,41 @@
  * @return {number}
  */
 var networkDelayTime = function(times, n, k) {
-    const time = Array(n).fill(Infinity)
-    time[k-1]=0
-    let changed = true
-    while(changed){
-        changed = false
-        for(let [node1,node2,timeToNode] of times){
-            node1--,node2--
-            if(time[node1]===Infinity) continue
-            if(time[node2] > time[node1] + timeToNode){
-                time[node2] = time[node1] + timeToNode
-                changed = true
-            }
+    let graph = new Array(n+1).fill(null);
+    for(let time of times){
+        const [from, to, t] = time;
+        if(!graph[from])
+            graph[from] = [];
+        graph[from].push([to,t]);
+    }
+    
+    let minHeap = new MinPriorityQueue({
+        priority: (item) => item[1]
+    });
+    const visited = new Set();
+    let minTime = 0;
+    minHeap.enqueue([k,0]);
+    
+    //BFS
+    while(minHeap.front() !== null){
+        const [node,totalTime] = minHeap.dequeue().element;
+        if(visited.has(node))
+            continue;
+        visited.add(node);
+        minTime = Math.max(minTime,totalTime);
+        
+        let connectedNodes = graph[node];
+        if(!connectedNodes)
+            continue;
+        for(let node of connectedNodes){
+            const [nodeNum, nodeTime] = node;
+            minHeap.enqueue([nodeNum,nodeTime + totalTime])
         }
     }
-    let res = Math.max(...time)
-    return res === Infinity ? -1 : res
+    
+    if(visited.size !== n){
+        return -1
+    }
+    
+    return minTime;
 };
