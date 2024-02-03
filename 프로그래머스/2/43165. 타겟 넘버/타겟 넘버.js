3cb009dc82000284661c7b36d8b66e1c3c8a3a65
jang siwoo <88125431+siwooJang@users.noreply.github.com>
2024-02-03 23:59:59 +0900

    [level 2] Title: 타겟 넘버, Time: 1.14 ms, Memory: 35.4 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -1,14 +1,14 @@
 function solution(numbers, target) {
   let answer = 0;
 
-  const dfs = (idx, sum) => {
-    if (idx === numbers.length) {
+  const dfs = (index, sum) => {
+    if (index === numbers.length) {
       if (sum === target) answer++;
       return;
     }
 
-    dfs(idx + 1, sum + numbers[idx]);
-    dfs(idx + 1, sum - numbers[idx]);
+    dfs(index + 1, sum + numbers[index]);
+    dfs(index + 1, sum - numbers[index]);
   };
 
   dfs(0, 0);
