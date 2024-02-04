e7689842c80211efcf43ca3cb70f0b8d56a1ba78
jang siwoo <88125431+siwooJang@users.noreply.github.com>
2024-02-05 00:47:17 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 30.07 ms, Memory: 63.4 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,10 +1,22 @@
 function solution(participant, completion) {
-    let p = participant.sort();
-    let c = completion.sort();
-    for(let i =0; i<p.length; i++){
-        if(p[i]!=c[i]){
-            return p[i]
+    let k = new Map();
+    for(let i=0;i<completion.length;i++){
+        if(!k.has(completion[i])){
+            k.set(completion[i],1)
+        } else {
+            k.set(completion[i],k.get(completion[i])+1)
         }
     }
     
+    for(let i=0;i<participant.length;i++){
+        if(!k.has(participant[i])){
+            return participant[i]
+        }
+        
+        if(k.get(participant[i])===0){
+            return participant[i]
+        } else {
+            k.set(participant[i],k.get(participant[i])-1)
+        }
+    }
 }
