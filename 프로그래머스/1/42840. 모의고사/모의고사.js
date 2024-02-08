d9d125c524acd8959f04b06963e05e7d57496863
jang siwoo <88125431+siwooJang@users.noreply.github.com>
2024-02-09 00:09:38 +0900

    [level 1] Title: 모의고사, Time: 3.56 ms, Memory: 36.8 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.js
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -1,18 +1,24 @@
 function solution(answers) {
-var answer = [];
-    var a1 = [1, 2, 3, 4, 5];
-    var a2 = [2, 1, 2, 3, 2, 4, 2, 5]
-    var a3 = [ 3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
+    
+    let result = [];
+    
+    let a1 = [1, 2, 3, 4, 5];
+    let a2 = [2, 1, 2, 3, 2, 4, 2, 5]
+    let a3 = [ 3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
+    
+    let counter = [0,0,0]
 
-    var a1c = answers.filter((a,i)=> a === a1[i%a1.length]).length;
-    var a2c = answers.filter((a,i)=> a === a2[i%a2.length]).length;
-    var a3c = answers.filter((a,i)=> a === a3[i%a3.length]).length;
-    var max = Math.max(a1c,a2c,a3c);
-
-    if (a1c === max) {answer.push(1)};
-    if (a2c === max) {answer.push(2)};
-    if (a3c === max) {answer.push(3)};
-
-
-    return answer;
+    for(let i =0; i<answers.length;i++){
+        if(answers[i]===a1[i%a1.length]) counter[0]++;
+        if(answers[i]===a2[i%a2.length]) counter[1]++;
+        if(answers[i]===a3[i%a3.length]) counter[2]++;
+    }
+    
+    let max = Math.max(counter[0],counter[1],counter[2])
+    
+    if(max===counter[0]) result.push(1);
+    if(max===counter[1]) result.push(2);
+    if(max===counter[2]) result.push(3);
+    
+    return result
 }
