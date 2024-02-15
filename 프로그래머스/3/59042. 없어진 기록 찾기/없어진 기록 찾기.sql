d290d37d650f81423fe67a1828e3101b1846ba6e
jang siwoo <88125431+siwooJang@users.noreply.github.com>
2024-02-15 23:57:39 +0900

    [level 3] Title: 없어진 기록 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
+++ 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
@@ -1,4 +1,4 @@
-select distinct a.animal_id,a.name
+select a.animal_id,a.name
 from animal_outs a left join animal_ins b on a.animal_id = b.animal_id
 where b.animal_id is null
 order by a.animal_id
