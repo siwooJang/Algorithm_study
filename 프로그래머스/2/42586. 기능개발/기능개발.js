ff749e66cdd3317ad269c9f1365e759bee45fbc8
jang siwoo <88125431+siwooJang@users.noreply.github.com>
2024-02-19 00:38:53 +0900

    [level 2] Title: 기능개발, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.js
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -1,26 +1,19 @@
 function solution(progresses, speeds) {
-    let count = 0;
     let result = [];
-
-    while (progresses.length > 0) {
-        // 각 기능의 진도 갱신
-        for (let i = 0; i < progresses.length; i++) {
+    let count = 0;
+    while(progresses.length>0){
+        for(let i =0; i<progresses.length; i++){
             progresses[i] += speeds[i];
         }
-
-        // 완료된 기능 배포
-        while (progresses.length > 0 && progresses[0] >= 100) {
+        while(progresses.length>0 && progresses[0] >= 100){
             progresses.shift();
             speeds.shift();
             count++;
         }
-
-        // 완료된 기능이 있을 경우 결과에 추가
-        if (count > 0) {
+        if(count>0){
             result.push(count);
-            count = 0; // 초기화
+            count = 0;
         }
     }
-
     return result;
 }
