3c6a9eb3193945fcf7001d171eb03f2909f37e8b
jang siwoo <88125431+siwooJang@users.noreply.github.com>
2024-03-01 23:57:18 +0900

    [level 3] Title: 정수 삼각형, Time: 5.87 ms, Memory: 41.7 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.js
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.js
@@ -1,14 +1,14 @@
 function solution(triangle) {
   for (let i = 1; i < triangle.length; i++) {
     for (let j = 0; j < triangle[i].length; j++) {
-      const firstCase = triangle[i - 1][j - 1] ?? 0;
-      const secondCase = triangle[i - 1][j] ?? 0;
+      const a = triangle[i - 1][j - 1] ?? 0;
+      const b = triangle[i - 1][j] ?? 0;
  
-      if (secondCase >= firstCase) {
-        triangle[i][j] += secondCase;
+      if (b >= a) {
+        triangle[i][j] += b;
         continue;
       }
-      triangle[i][j] += firstCase;
+      triangle[i][j] += a;
     }
   }
   return Math.max(...triangle.at(-1));
