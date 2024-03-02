90bd78b42e2584e085ce4c0ffe37d2b5a48cdb49
jang siwoo <88125431+siwooJang@users.noreply.github.com>
2024-03-02 23:42:32 +0900

    [level 3] Title: N으로 표현, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.js
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.js
@@ -1,23 +1,23 @@
 function solution(N, number) {
     var answer = 0;
-    var use = Array.from(new Array(9), () => new Set());
+    var a = Array.from(new Array(9), () => new Set());
     if(N==number) return 1;
     else {
-        use.forEach((element, index) => {
+        a.forEach((element, index) => {
             if (index !== 0) element.add(Number(String(N).repeat(index)));
         });
         for(var i=1 ; i<=8 ; ++i) {
             for(var j=1 ; j<i ; ++j) {
-                for(var first of use[j]) {
-                    for(var second of use[i-j]) {
-                        use[i].add(first+second);
-                        use[i].add(first-second);
-                        use[i].add(first*second);
-                        use[i].add(first/second);
+                for(var first of a[j]) {
+                    for(var second of a[i-j]) {
+                        a[i].add(first+second);
+                        a[i].add(first-second);
+                        a[i].add(first*second);
+                        a[i].add(first/second);
                     }
                 } 
             }
-            if(use[i].has(number)) return i;
+            if(a[i].has(number)) return i;
         }
         return -1;
     }
