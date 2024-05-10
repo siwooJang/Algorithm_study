7b41a71bc474b2281bdd76a26d6dd418a72a28c9
jang siwoo <88125431+siwooJang@users.noreply.github.com>
2024-05-10 23:00:52 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1464 ms, Memory: 136480 KB -BaekjoonHub

---
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
@@ -0,0 +1,20 @@
+import bisect
+
+def count_match(mnums,nums):
+    nums.sort()
+    result = []
+    
+    for num in mnums:
+        left = bisect.bisect_left(nums,num)
+        right = bisect.bisect_right(nums,num)
+        count = right - left
+        result.append(count)
+    return result
+
+N = int(input()) 
+nums = list(map(int, input().split()))  
+M = int(input()) 
+mnums = list(map(int, input().split())) 
+
+result = count_match(mnums,nums)
+print(' '.join(map(str,result)))
