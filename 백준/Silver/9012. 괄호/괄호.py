0174881affd0e59b4e37032a7c281352fc14616b
jang siwoo <88125431+siwooJang@users.noreply.github.com>
2024-05-15 23:43:38 +0900

    [Silver IV] Title: 괄호, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.py
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -1,19 +1,19 @@
-T = int(input())
-
-for i in range(T):
+t = int(input())
+for _ in range(t):
     stack = []
-    a=input()
-    for j in a:
-        if j == '(':
-            stack.append(j)
-        elif j == ')':
+    s = input()
+    isVPS = True
+    
+    for ch in s:
+        if ch =='(':
+            stack.append('(')
+        if ch ==')':
             if stack:
                 stack.pop()
-            else: # 스택에 괄호가 없을경우 NO
-                print("NO")
+            elif not stack:
+                isVPS= False
                 break
-    else: # break문으로 끊기지 않고 수행됬을경우 수행한다
-        if not stack: # break문으로 안끊기고 스택이 비어있다면 괄호가 다 맞는거다.
-            print("YES")
-        else: # break안 걸렸더라도 스택에 괄호가 들어있다면 NO이다.
-            print("NO")
+    if not stack and isVPS:
+        print('YES')
+    elif stack or not isVPS:
+        print('NO')
